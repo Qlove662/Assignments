ef188a6859ae7b2f6a742389b43bc5c3901989ec
Qlove662 <love.jones84@yahoo.com>
2024-10-14 22:50:24 -0500

    resubmit calculator assignment

---
+++ Javascript_functions/script.js
@@ -0,0 +1,25 @@
+function handleAddition () {
+    let number1 = Number(document.getElementById("number1").value)
+    let number2 = Number(document.getElementById("number2").value)
+    document.getElementById("result").value = number1 + number2
+  }
+  
+  function handleSubtraction () {
+    let number1 = Number(document.getElementById("number1").value)
+    let number2 = Number(document.getElementById("number2").value)
+    document.getElementById("result").value = number1 - number2
+  }
+  
+  function handleMultipication () {
+    let number1 = Number(document.getElementById("number1").value)
+    let number2 = Number(document.getElementById("number2").value)
+    document.getElementById("result").value = number1 * number2
+  }
+  
+  function handleDivision () {
+    let number1 = Number(document.getElementById("number1").value)
+    let number2 = Number(document.getElementById("number2").value)
+    document.getElementById("result").value = number1 / number2
+  }
+  
+  
